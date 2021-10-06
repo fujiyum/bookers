b7cb5af48d0b89650cbf9f6db0d31aa041271783
fujiyum <91727339+fujiyum@users.noreply.github.com>
2021-10-06 12:17:54 +0000

    [3-9]Update

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,13 +1,44 @@
 class BooksController < ApplicationController
-  def index
-  end
 
-  def show
-  end
+    def index
+      @books = Book.all
+      @book = Book.new
+    end
 
-  def new
-  end
+    def create
+      @book = Book.new(book_params)
 
-  def edit
-  end
+        if @book.save
+          redirect_to book_path(@book.id)
+        else
+          render index_path
+        end
+
+    end
+
+    def show
+      @book = Book.find(params[:id])
+    end
+
+    def edit
+      @book = Book.find(params[:id])
+    end
+
+    def update
+      book = book.find(parame[:id])
+      book.update(book_params)
+      redirect_to book_path(book)
+    end
+
+    def destroy
+      book = Book.find(params[:id])
+      book.destroy
+      redirect_to books_path
+    end
+
+      private
+      def book_params
+          params.require(:book).permit(:title, :body)
+      end
+
 end
--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -3,28 +3,4 @@ class HomesController < ApplicationController
     def top
     end
 
-    def index
-        @books = Book.all
-    end
-
-    def new
-        @book = Book.new
-    end
-
-    def create
-        @book = Book.new(@book_params)
-
-        if @book.save
-        redirect_to book_path(@book.id)
-        else
-        render index_path
-        end
-
-    end
-
-    private
-    def book_params
-        params.require(:book).permit(:title, :body)
-    end
-
 end
